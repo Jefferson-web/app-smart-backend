d86d74838b09c744d835d8c384d9891362bbbbfa
Jeffe <Jeffe@DESKTOP-KPPTAJB>
2021-12-05 10:44:19 -0500

    mapper

---
+++ AppSmartDoctor/Mapping/SmProfile.cs
@@ -0,0 +1,19 @@
+﻿using AppSmartDoctor.Models;
+using AppSmartDoctor.Models.ViewModel;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppSmartDoctor.Mapping
+{
+    public class SmProfile: Profile
+    {
+
+        public SmProfile() {
+            CreateMap<Paciente, PacienteDTO>().ReverseMap();
+        }
+
+    }
+}
---
+++ AppSmartDoctor/Models/ViewModel/PacienteDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppSmartDoctor.Models.ViewModel
+{
+    public class PacienteDTO
+    {
+        public string nombres { get; set; }
+        public string email { get; set; }
+        public string contrasena { get; set; }
+        public string DNI { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime fecha_nacimiento { get; set; }
+        public int edad { get; set; }
+        public bool sexo { get; set; }
+        public string distrito_colonia { get; set; }
+    }
+}
